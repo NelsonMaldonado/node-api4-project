9a299f26ccb62749ec3e4a5afa45bbdfe09c081e
Nelson Maldonado <play2nino@gmail.com>
2022-01-13 13:09:56 -0800

    wip

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+require("dotenv").config()
+const path = require("path")
+
 const express = require("express")
+const { append } = require("express/lib/response")
 const server = express()
 const port = process.env.PORT || 9000
+
+server.use(express.json())
 
 server.get("/", (req, res) => {
   res.send("Default endpoint is working")

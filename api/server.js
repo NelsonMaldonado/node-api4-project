5e04798d6a72306c45fa4c3ba800f337e0c924a5
Nelson Maldonado <play2nino@gmail.com>
2022-01-13 13:19:39 -0800

    default api working

--- api/server.js
+++ api/server.js
@@ -1,1 +1,12 @@
+require("dotenv").config
+
 const express = require("express")
+const server = express()
+const port = process.env.PORT || 9000
+server.use(express.json())
+
+server.get("/", (req, res) => {
+  res.send("<h1>Default endpoint is working</h1><h3>body info</h3> ")
+})
+
+module.exports = server
--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 require("dotenv").config()
-const path = require("path")
-
-const express = require("express")
-const { append } = require("express/lib/response")
-const server = express()
+const server = require("./api/server")
 const port = process.env.PORT || 9000
-
-server.use(express.json())
-
-server.get("/", (req, res) => {
-  res.send("Default endpoint is working")
-})
-
 server.listen(port, () => {
   console.log(`/n****Listening on port: ${port}****n/`)
 })
